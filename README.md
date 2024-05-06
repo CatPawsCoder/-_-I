6f87806974a8b413b78a248ad617076de273f2f7
Meme <dariabespal@gmail.com>
2024-05-06 21:34:03 +0300

    streamlit app + uvicorn api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# -_-I
+#Excercises streamlit || api with uvicorn
+Face parsing
+
+App utilized face parsing model jonathandinu/face-parsing and streamlit to get semantic segmentation of face
+
+ https://github.com/sozykin/streamlit_demo_app/blob/main/image_classification.py
+
+ API
+ sentiment analysis
---
+++ api_excercise.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    text: str
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+
+@app.get ("/")
+async def root ():
+    return {"message": "Hello world"}
+
+@app.post("/predict/")
+def predict(item: Item):
+     """Sentiment analysis for a text"""
+     return classifier(item.text )[0]
+
+
+# @app.get("/predict/")
+# def predict ():
+#     return classifier("I don't like machine learning engineering!")
